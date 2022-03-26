c3fb3652bfadeb88e5b3c94a357982097b817a92
hope <hope@gravylab.co.kr>
2022-03-26 18:28:30 +0900

    test 추가

--- src/test/java/com/icloud/filter/KoreanNumberTest.java
+++ src/test/java/com/icloud/filter/KoreanNumberTest.java
@@ -8,10 +8,10 @@ import org.logstash.plugins.ContextImpl;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertInstanceOf;
 
 class KoreanNumberTest {
 
@@ -38,6 +38,25 @@ class KoreanNumberTest {
 
         assertEquals(String.valueOf(35_480_000), event.getField("field1"));
         assertEquals(String.valueOf(554_300_000), event.getField("field2"));
+    }
+
+    @Test
+    void korean_number_filter_tag_test() {
+        var fieldList = List.of("field");
+
+        var configMap = new HashMap<String, Object>(Collections.singletonMap("field", fieldList));
+        Configuration config = new ConfigurationImpl(configMap);
+        Context context = new ContextImpl(null, null);
+
+        Filter filter = new KoreanNumber("test-id", config, context);
+        Event event = new org.logstash.Event();
+        FilterMatchListener filterMatchListener = new TestMatchListener();
+        filter.filter(Collections.singletonList(event), filterMatchListener);
+        event.setField("field", 345234);
+        assertInstanceOf(List.class, event.getField("tags"));
+        assertEquals(((List<?>) event.getField("tags")).get(0), "wrong value on field [field]");
+
+
     }
 
     static class TestMatchListener implements FilterMatchListener {
